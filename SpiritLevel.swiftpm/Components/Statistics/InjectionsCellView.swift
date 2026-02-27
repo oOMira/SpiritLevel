61c931432971c5b284129fc781186113aa67a097
oOMira <git@mira.email>
2026-02-27 03:49:38 +0100

    some cleanup

--- SpiritLevel.swiftpm/Components/Statistics/InjectionsCellView.swift
+++ SpiritLevel.swiftpm/Components/Statistics/InjectionsCellView.swift
@@ -1,25 +1,29 @@
 import SwiftUI
 
 struct InjectionsCellView: View {
+    @State private var expanded: Bool = false
     var injections: [Injection]
     
     var body: some View {
         Group {
-            ForEach(injections) { injection in
+            ForEach(expanded ? injections : Array(injections.prefix(.maxInjectionsToShow))) { injection in
                 Text("\(injection.dosage, specifier: .doseageFormat) mg EE on \(injection.date, format: .dateTime.day().month().year())")
             }
         }
         .accessibilityElement(children: .contain)
-        Button(.showMoreButton) {
-            print("show more injections")
-        }
+        Button(action: {
+            withAnimation { expanded.toggle() }
+        }, label: {
+            AccessibleCollapseButton(expanded: $expanded)
+        })
     }
 }
+    
 
 // MARK: - Constants
 
-private extension LocalizedStringResource {
-    static let showMoreButton: Self = "Show more"
+private extension Int {
+    static let maxInjectionsToShow: Self = 3
 }
 
 private extension String {
