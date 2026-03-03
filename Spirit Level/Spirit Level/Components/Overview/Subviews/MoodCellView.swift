8f1d8dcad737c1e316b8655ee884ad49f945e5a2
oOMira <git@mira.email>
2026-03-04 00:59:40 +0100

    replace with lottie

--- Spirit Level/Spirit Level/Components/Overview/Subviews/MoodCellView.swift
+++ Spirit Level/Spirit Level/Components/Overview/Subviews/MoodCellView.swift
@@ -1,7 +1,11 @@
 import SwiftUI
+import Lottie
 
 struct MoodCellView<InjectionRepositoryType: InjectionManageable,
                     HormoneManagerType: HormoneLevelManageable>: View {
+    
+    @Environment(\.accessibilityReduceMotion) var reduceMotion
+    @State private var playing: Bool = false
     
     let injectionRepository: InjectionRepositoryType
     let hormoneManager: HormoneManagerType
@@ -10,7 +14,21 @@ struct MoodCellView<InjectionRepositoryType: InjectionManageable,
     var body: some View {
         let currentMood = self.mood
         return VStack(alignment: .center) {
-            MoodView(mood: currentMood)
+            LottieView(animation: .named(currentMood.resourceName))
+                .playbackMode(
+                    reduceMotion
+                        ? (!playing ? .paused(at: .progress(0)) : .playing(.fromProgress(0, toProgress: 1, loopMode: .playOnce)))
+                        : .playing(.fromProgress(0, toProgress: 1, loopMode: .loop))
+                )
+                .animationDidFinish { _ in
+                    guard reduceMotion else { return }
+                    playing = false
+                }
+                .reloadAnimationTrigger(playing, showPlaceholder: false)
+                .onTapGesture {
+                    guard reduceMotion else { return }
+                    playing = true
+                }
                 .frame(width: .moodViewSize, height: .moodViewSize)
         }
         .frame(maxWidth: .infinity)
@@ -25,6 +43,18 @@ struct MoodCellView<InjectionRepositoryType: InjectionManageable,
             return .unclear
         } else {
             return  isFalling ? .sad : .happy
+        }
+    }
+}
+
+// MARK: - Helper
+
+private extension Mood {
+    var resourceName: String {
+        switch self {
+        case .happy: "smilecat"
+        case .sad: "cryingcat"
+        case .unclear: "smileycat"
         }
     }
 }
